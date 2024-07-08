001eb4c92fdb3f36be9a911f6eca498429932ad1
Shristi Sehwag <91873414+ShristiSehwag@users.noreply.github.com>
2024-07-08 09:01:09 +0000

    Basics

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+# Define the file name 
+file_name = "automated_file.txt"
+
+# Define the text to write 
+text = "Hello! This is an automated file with text."
+
+# Open the file in write mode (this will create the file if it doesn't exist) 
+with open(file_name, 'w') as file: 
+    file.write(text) 
+
+print(f"File '{file_name}' has been created and the message has been written.")
+
